8ff25c2cbffa538b0a1ce063a78fa0d5921c2e14
Roberto Sousa <beto.sousa22@gmail.com>
2017-03-20 14:43:16 +0000

    exucute the dll file from a request and pass some parts into it

--- server/src/client/client-controller.ts
+++ server/src/client/client-controller.ts
@@ -7,15 +7,29 @@ import fs = require('fs');
 
 const app = require('../application');
 
-var helloWorld = edge.func(`
-	async (input) => {
-		return ".NET Welcomes " + input.ToString();
-	}
-`);
+// const createNewClientFunc = edge.func(`
+// 	async (input) => {
+// 		return ".NET Welcomes " + input.ToString();
+// 	}
+// `);
 
+var createNewClientFunc = edge.func('new.client.dll');
+
+
+// export function createNewClient(req: express.Request, res: express.Response): void {
+// 	createNewClientFunc('javascript', function (error, result) {
+// 		if (error) {
+// 			res.status(500).send('Error executing the createClientEdge function');
+// 			return;
+// 		};
+// 		console.log('result-->', result);//roberto
+// 		res.status(200).send('New client added');
+// 		return;
+// 	});
+// }
 
 export function createNewClient(req: express.Request, res: express.Response): void {
-	helloWorld('javascript', function (error, result) {
+	createNewClientFunc(req.body, function (error, result) {
 		if (error) {
 			res.status(500).send('Error executing the createClientEdge function');
 			return;
