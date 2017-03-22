7b2641a4babda724c69d607bd6dc746718f3606c
Roberto Sousa <beto.sousa22@gmail.com>
2017-03-22 17:08:22 +0000

    import the dll files to the c# code

--- server/src/client/client-controller.ts
+++ server/src/client/client-controller.ts
@@ -1,4 +1,3 @@
-
 import edge = require('edge');
 import express = require('express');
 import path = require('path');
@@ -7,26 +6,28 @@ import fs = require('fs');
 
 const app = require('../application');
 
-// const createNewClientFunc = edge.func(`
-// 	async (input) => {
-// 		return ".NET Welcomes " + input.ToString();
-// 	}
-// `);
+ const createNewClientFunc = edge.func(`
+	#r "dll/RTLBL16.dll"
+	#r "dll/RTLSystem16.dll"
+	#r "dll/RTLUtil16.dll"
+	#r "dll/RTLBase16.dll"
+	#r "dll/RTLDL16.dll"
+	#r "dll/RTLPrint16.dll"
+	using System;
+	using System.Threading.Tasks;
+	using System.Collections.Generic;
+	using System.ComponentModel;
+	using System.Drawing;
+	using System.Linq;
+	using System.Text;
+	using System.Runtime.InteropServices;
+	public class Startup
+	{
+		public async Task<object> Invoke(dynamic input) {
+			return ".NET Welcomes " + input.ToString();
+		}
+	}`);
 
-var createNewClientFunc = edge.func('new.client.dll');
-
-
-// export function createNewClient(req: express.Request, res: express.Response): void {
-// 	createNewClientFunc('javascript', function (error, result) {
-// 		if (error) {
-// 			res.status(500).send('Error executing the createClientEdge function');
-// 			return;
-// 		};
-// 		console.log('result-->', result);//roberto
-// 		res.status(200).send('New client added');
-// 		return;
-// 	});
-// }
 
 export function createNewClient(req: express.Request, res: express.Response): void {
 	createNewClientFunc(req.body, function (error, result) {
